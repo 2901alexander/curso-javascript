df22d3c4520dbdaf70082fa7c1f879983aa54564
2901alexander <ncygm@hotmail.com>
2022-03-14 16:12:51 -0500

    medidas del circulo

--- figuras.js
+++ figuras.js
@@ -1,1 +1,31 @@
-alert ("hello world");
+console.group("Circulo");
+
+// Radio
+const radioCirculo = 4;
+console.log("El radio del circulo es:" + radioCirculo + "cm");
+// Diametro
+const diametroCirculo = radioCirculo * 2;
+console.log("El diametro del circulo es:" + diametroCirculo + "cm");
+// PI
+  //const PI = 3.1415;
+const PI = Math.PI; 
+console.log("PI es:" + PI);
+// Circunferencia 
+const perimetroCirculo = diametroCirculo * PI;
+console.log("El perimetro del circulo es:" + perimetroCirculo + "cm");
+// Area
+const areaCirculo = (radioCirculo * radioCirculo) * PI;
+console.log("El area del circulo es:" + areaCirculo + "cm^2");
+console.groupEnd();
+
+const numeroPI = Math.PI;
+function diametro (radio){
+    return radio * 2;
+};
+
+
+function perimetroCirculof (radio){
+   const diamet = diametro (radio);
+   return diamet * numeroPI;
+
+};
