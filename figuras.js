4ef1e468b53670781ac795c57ee4e90966ec5431
2901alexander <ncygm@hotmail.com>
2022-03-14 22:17:28 -0500

    triangulo isosceles

--- figuras.js
+++ figuras.js
@@ -81,3 +81,19 @@ function calcularAreaTriangulo(){
     const areaTrian = (base * altura) / 2;
     alert(areaTrian);
 }
+
+// RETO TRIANGULO ISOSCELES 
+
+function altura(lado1, lado2, base){
+    if(lado1 === lado2 && lado1 != base){
+        alert('isoceles')
+        const altura = Math.sqrt(lado1**2 - base**2/ 4)
+        console.log(altura);
+        alert(altura);
+    }
+    else{
+        alert('no lo se')
+    }
+}
+altura(6, 6, 4)
+
